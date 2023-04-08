44bf69328b2157c4b801ad73b7efc202d6c93a73
Mykhaylo Zelisko <zelisko.mykhaylo@gmail.com>
2023-04-08 12:35:33 +0300

    add liners for BE and fix linter for FE

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,113 +1,107 @@
 module.exports = {
-  'root': true,
-  'ignorePatterns': [
+  root: true,
+  ignorePatterns: [
     'projects/**/*'
   ],
-	'env': {
-		'browser': true,
-		'es2021': true
+	env: {
+		browser: true,
+		es2021: true
 	},
-	'extends': [],
-	'overrides': [
+	extends: [],
+	overrides: [
     {
-      "files": [
-        "*.ts"
+      files: [
+        '*.ts'
       ],
-      "parserOptions": {
-        "project": [
-          "tsconfig.json"
+      parserOptions: {
+        project: [
+          'tsconfig.json'
         ],
-        "createDefaultProgram": true
+        createDefaultProgram: true
       },
-      "extends": [
-        "plugin:@angular-eslint/recommended",
-        "plugin:@angular-eslint/template/process-inline-templates",
-        "plugin:@typescript-eslint/recommended",
-        "airbnb-typescript/base",
-        "prettier",
-        "plugin:prettier/recommended"
+      extends: [
+        'plugin:@angular-eslint/recommended',
+        'plugin:@angular-eslint/template/process-inline-templates',
+        'plugin:@typescript-eslint/recommended',
+        'airbnb-typescript/base',
+        'prettier',
+        'plugin:prettier/recommended'
       ],
-      "rules": {
-        "@angular-eslint/directive-selector": [
-          "error",
+      rules: {
+        '@angular-eslint/directive-selector': [
+          'error',
           {
-            "type": "attribute",
-            "prefix": "app",
-            "style": "camelCase"
+            type: 'attribute',
+            prefix: 'app',
+            style: 'camelCase'
           }
         ],
-        "@angular-eslint/component-selector": [
-          "error",
+        '@angular-eslint/component-selector': [
+          'error',
           {
-            "type": "element",
-            "prefix": "app",
-            "style": "kebab-case"
+            type: 'element',
+            prefix: 'app',
+            style: 'kebab-case'
           }
         ],
-        "@typescript-eslint/typedef": [
-          "warn",
+        '@typescript-eslint/typedef': [
+          'warn',
           {
-            "call-signature": true,
-            "parameter": true,
-            "arrow-parameter": true,
-            "property-declaration": true,
-            "variable-declaration": true,
-            "member-variable-declaration": true,
-            "object-destructuring": true,
-            "array-destructuring": true
+            'call-signature': true,
+            parameter: true,
+            'arrow-parameter': true,
+            'property-declaration': true,
+            'variable-declaration': true,
+            'member-variable-declaration': true,
+            'object-destructuring': true,
+            'array-destructuring': true
           }
         ],
-        "prettier/prettier": [
-          "error",
+        'prettier/prettier': [
+          'error',
           {
-            "endOfLine": "auto"
+            endOfLine: 'auto'
           }
         ],
-        "import/extensions": "off",
-        "import/no-extraneous-dependencies": "off",
-        "import/no-unresolved": "off",
-        "import/prefer-default-export": "off",
-        "class-methods-use-this": "off",
-        "lines-between-class-members": "off",
-        "no-var": "error",
-        "no-debugger": "error",
-        "no-duplicate-imports": "error",
-        "no-duplicate-case": "error",
-        "prefer-const": "warn",
-        "quotes": [
-          "warn",
-          "single"
-        ],
-        "curly": "warn",
-        "eqeqeq": [
-          "warn",
-          "smart"
-        ],
-        "no-magic-numbers": ["warn", { "ignore": [-1, 0, 1, 2]}],
-        "@typescript-eslint/no-inferrable-types": "off",
-        "@typescript-eslint/explicit-member-accessibility": ["warn"]
+        'import/extensions': 'off',
+        'import/no-extraneous-dependencies': 'off',
+        'import/no-unresolved': 'off',
+        'import/prefer-default-export': 'off',
+        'class-methods-use-this': 'off',
+        'lines-between-class-members': 'off',
+        'no-var': 'error',
+        'no-debugger': 'error',
+        'no-duplicate-imports': 'error',
+        'no-duplicate-case': 'error',
+        'prefer-const': 'warn',
+        quotes: ['warn', 'single'],
+        curly: 'warn',
+        eqeqeq: ['warn', 'smart'],
+        'no-magic-numbers': ['warn', { 'ignore': [-1, 0, 1, 2]}],
+        '@typescript-eslint/no-inferrable-types': 'off',
+        '@typescript-eslint/explicit-member-accessibility': ['warn']
       }
     },
     {
-      "files": [
-        "*.html"
+      files: [
+        '*.html'
       ],
-      "extends": [
-        "plugin:@angular-eslint/template/recommended",
-        "prettier",
-        "plugin:prettier/recommended"
+      extends: [
+        'plugin:@angular-eslint/template/recommended',
+        'prettier',
+        'plugin:prettier/recommended'
       ],
-      "rules": {
-        "prettier/prettier": ["warn"]
+      rules: {
+        'prettier/prettier': ['warn']
       }
     }
 	],
-	'parser': '@typescript-eslint/parser',
-	'parserOptions': {
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
 		'ecmaVersion': 'latest'
 	},
-	'plugins': [
+	plugins: [
 		'@typescript-eslint'
 	],
-	'rules': {}
+	rules: {}
 };
