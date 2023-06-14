8037be956dad3c0b280a0142d971b60d04e7c981
Mykhaylo Zelisko <zelisko.mykhaylo@gmail.com>
2023-06-14 17:39:01 +0300

    refactor header

--- client/src/app/layout/components/header/header.component.ts
+++ client/src/app/layout/components/header/header.component.ts
@@ -6,24 +6,27 @@ import {
   OnInit,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, RouterLink } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { UserInterface } from '../../../shared/models/interfaces/user.interface';
 import { UsersService } from '../../../shared/services/users.service';
 import { Subject, takeUntil } from 'rxjs';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { UserRouteNameEnum } from '../../../shared/models/enums/user-route-name.enum';
 import { LayoutRouteNameEnum } from '../../../shared/models/enums/layout-route-name.enum';
+import { ClickOutsideDirective } from '../../../shared/directives/click-outside.directive';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterLink, AngularSvgIconModule],
+  imports: [CommonModule, RouterLink, AngularSvgIconModule, ClickOutsideDirective, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent implements OnInit, OnDestroy {
   public user: UserInterface | null = null;
+
+  public isActiveDropDown: boolean = false;
 
   private destroy$: Subject<void> = new Subject<void>();
 
@@ -56,5 +59,14 @@ export class HeaderComponent implements OnInit, OnDestroy {
     ) {
       this.router.navigateByUrl('');
     }
+    this.isActiveDropDown = false;
+  }
+
+  public openDropDown(): void {
+    this.isActiveDropDown = !this.isActiveDropDown;
+  }
+
+  public closeDropDown(): void {
+    this.isActiveDropDown = false;
   }
 }
