d0853d2bc56b86a09db099c0b6a2165984a81ab3
Mykhaylo Zelisko <zelisko.mykhaylo@gmail.com>
2023-07-15 16:53:36 +0300

    add alert

---
+++ client/src/app/admin/pages/new-article/components/alert/alert.component.ts
@@ -0,0 +1,19 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+
+@Component({
+  selector: 'app-alert',
+  standalone: true,
+  imports: [CommonModule, AngularSvgIconModule],
+  templateUrl: './alert.component.html',
+  styleUrls: ['./alert.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AlertComponent {
+  public isAlertVisible: boolean = true;
+
+  public toggleAlert(): void {
+    this.isAlertVisible = !this.isAlertVisible;
+  }
+}
