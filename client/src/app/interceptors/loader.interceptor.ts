b48311d1e133ea6674ed36f892c899b34a1ed0c3
Mykhaylo Zelisko <zelisko.mykhaylo@gmail.com>
2023-10-20 22:59:23 +0300

    add loader

---
+++ client/src/app/interceptors/loader.interceptor.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { finalize, Observable } from 'rxjs';
+import { LoaderService } from '../shared/services/loader.service';
+
+const EXCEPTION_ROUTES: string[] = ['/users'];
+
+@Injectable()
+export class LoaderInterceptor implements HttpInterceptor {
+  public constructor(private loaderService: LoaderService) {}
+
+  public intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler,
+  ): Observable<HttpEvent<unknown>> {
+    if (EXCEPTION_ROUTES.some((i) => request.url.includes(i))) {
+      console.log(request.url, 1);
+      return next.handle(request);
+    } else {
+      console.log(request.url, 2);
+      this.loaderService.show();
+
+      return next
+        .handle(request)
+        .pipe(finalize(() => this.loaderService.hide()));
+    }
+  }
+}
