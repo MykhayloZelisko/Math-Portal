03e43ba65fa0dbb43cf72946d7e1ee9fac296055
Mykhaylo Zelisko <zelisko.mykhaylo@gmail.com>
2023-11-08 00:14:11 +0200

    add unit tests

--- client/src/app/admin/pages/new-article/components/alert/alert.component.spec.ts
+++ client/src/app/admin/pages/new-article/components/alert/alert.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AlertComponent } from './alert.component';
 import { SvgIconRegistryService } from 'angular-svg-icon';
+
 describe('AlertComponent', () => {
   let component: AlertComponent;
   let fixture: ComponentFixture<AlertComponent>;
